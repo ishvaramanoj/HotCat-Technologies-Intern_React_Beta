f7cb6e922b8ee8ac87fd577e5d1b791fa39cf447
Ishvara <ishvaramanoj@gmail.com>
2023-11-20 22:35:05 +0530

    ccccommit

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import './App.css'
 import React from 'react'
 import Home from './Components/Pages/Home/Home'
 import Login from './Components/Pages/Login/Login'
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import ReactDOM from "react-dom/client";
 
 function App() {
  
 
   return (
     <>
-      <div>
+      <div className='app'>
+     
+
         {/* <Home/> */}
         <Login/>
-
+        
        </div>
     </>
   )
