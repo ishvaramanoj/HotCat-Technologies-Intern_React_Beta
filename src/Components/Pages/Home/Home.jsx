83c13e07c3f6764ba3b9f550f5119c1c556cfe2d
Ishvara <ishvaramanoj@gmail.com>
2023-11-20 11:45:34 +0530

    dcommit

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 import './Home.css'
-
+import InfoIcon from '@mui/icons-material/Info';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import CallIcon from '@mui/icons-material/Call';
+import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+import Avatar from '@mui/material/Avatar';
 
 const Home=()=> {
 
@@ -14,10 +20,20 @@ const Home=()=> {
 <div className='topheader'>
 
 <div className='topheaderleft'>
-    leftside
+<Avatar id='avatar'alt="COMPANY"/>
+<Button id='aboutbtn' variant="outlined">ABOUT<InfoIcon/></Button>
+<Button id='pricingbtn' variant="outlined">PRICING<AttachMoneyIcon/></Button>
+<Button id='contactbtn' variant="outlined">CONTACT<CallIcon/></Button>
+<Button id='supportbtn' variant="outlined">SUPPORT<SupportAgentIcon/></Button>
 </div>
+
+
+
 <div className='topheaderright'>
-    rightside
+    
+<Button id='loginbtn' variant="outlined">LOGIN</Button>
+<Button id='registerbtn' variant="outlined">REGISTER</Button>
+
 </div>
 
 </div>
