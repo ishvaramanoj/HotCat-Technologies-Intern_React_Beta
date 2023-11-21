026e1d561796898d9d542797da0484842566095f
Ishvara <ishvaramanoj@gmail.com>
2023-11-21 20:42:27 +0530

    ffcommit

--- src/Components/Pages/Dashboard/Dashboard.jsx
+++ src/Components/Pages/Dashboard/Dashboard.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import * as React from 'react';
 import './Dashboard.css'
-
-
+import Button from '@mui/material/Button';
+import GroupIcon from '@mui/icons-material/Group';
+import AddReactionIcon from '@mui/icons-material/AddReaction';
 
 const Dashboard=()=> {
 
 
   return (
+<>
 
+<div className='maindashboardtop'>
+<div className='topleft'>C.M.S DASHBOARD</div>
 
-<div className='maindashboard'>
-Dashboard
+<div className='topright'>
+<Button id='cstviewbtn' variant="contained" startIcon={<GroupIcon />}>Customer View</Button> 
 </div>
 
+<div className='topright2'>
+<Button id='cstactionbtn' variant="contained" startIcon={<AddReactionIcon />}>Customer Action</Button> 
+</div>
+
+
+
+</div>
+
+</>
 
   )
 
