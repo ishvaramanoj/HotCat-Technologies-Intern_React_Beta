593dfa60b7c5179bb573d37f33003c553e6d9fe9
Ishvara <ishvaramanoj@gmail.com>
2023-11-22 15:03:13 +0530

    sscommit

--- src/Components/Pages/Login/Login.jsx
+++ src/Components/Pages/Login/Login.jsx
@@ -3,6 +3,7 @@ import './Login.css'
 import EmailIcon from '@mui/icons-material/Email';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2'
 
 
 const Login=()=> {
@@ -10,7 +11,19 @@ const Login=()=> {
 const navigate = useNavigate()
 
 const lognow=()=>{
-  navigate('/Dashboard');
+  let Username =  document.getElementById("txtusername").value;
+  let Password =  document.getElementById("txtpassword").value;
+  if(Username=='customer'&&Password=='123'){
+    navigate('/Dashboard');
+  }else{
+    Swal.fire({
+      icon: "error",
+      title: "Invalid login",
+      text: "Please check the Username and Password entered",
+      footer: '<a href="#">Check whether the capslock is on</a>'
+    });
+  }
+  
 }
 
   return (
@@ -31,8 +44,8 @@ Sign Into Your C.M.S
 
  <div className='rightbody'>
 
- <input  type='text' className='username'  placeholder= 'Username' ></input><br></br>
- <input type='password' className='password' placeholder='Password'></input><br></br>
+ <input id='txtusername' type='text' className='username'  placeholder= 'Username' ></input><br></br>
+ <input id='txtpassword' type='password' className='password' placeholder='Password'></input><br></br>
  <p className='forgot'><a href=''>Forgot Password</a></p>
 
  <Button id='loginbutton' variant="contained" onClick={()=>{lognow()}}>LOGIN</Button>
