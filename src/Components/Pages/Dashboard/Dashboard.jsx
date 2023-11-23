c867e69aad073c8c20ce7e4f7278258b977aac5c
Ishvara <ishvaramanoj@gmail.com>
2023-11-23 14:56:05 +0530

    modalcommit

--- src/Components/Pages/Dashboard/Dashboard.jsx
+++ src/Components/Pages/Dashboard/Dashboard.jsx
@@ -15,7 +15,23 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import UpdateIcon from '@mui/icons-material/Update';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { useNavigate } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
+
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 
 
@@ -107,6 +123,13 @@ const columns = [
 
 const Dashboard=()=> {
 
+
+const [open, setOpen] = React.useState(false);
+const handleOpen = () => setOpen(true);
+const handleClose = () => setOpen(false);
+
+
+
 const navigate = useNavigate()
 
 const logout=()=>{
@@ -129,10 +152,31 @@ const logout=()=>{
 <>
 
 <div className='maindashboardtop'>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2" className='popuptitle'>
+            Add a Customer here
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+
+
+
+
+
 <div className='topleft'>C.M.S DASHBOARD</div>
 
 <div className='topright'>
-<Button id='cstviewbtn' variant="contained" startIcon={<GroupIcon />}>Add Customer</Button> 
+<Button id='cstviewbtn' variant="contained" startIcon={<GroupIcon />} onClick={handleOpen}>Add Customer</Button> 
 </div>
 
 <div className='topright2'>
